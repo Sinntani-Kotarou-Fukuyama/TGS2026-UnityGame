02297c6413be2b4981f3219974d5f3ebc812543a
Shintani-Kotaro-Fukuyama <gc25011@anabuki-net.ne.jp>
2026-05-29 10:15:58 +0900

    CameraShakeの追加
    CameraShakeで任意のタイミングで揺れを変更できるように

---
+++ Tightrope Game/Assets/Script/CameraShake.cs
@@ -0,0 +1,53 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    [Header("シェイクしたいもの")]
+    [SerializeField] private CinemachineBasicMultiChannelPerlin shake;
+    [Header("揺れの強さ/揺れの速さ/揺れの時間")]
+    [SerializeField] private float amplitudeGain = 1;//揺れの強さ
+    [SerializeField] private float frequencyGain = 1;//揺れの速さ
+    [SerializeField] private float shakeDuration = 1;//揺れの時間
+
+    CinemachineBasicMultiChannelPerlin perlin;
+    private float shakeTimer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        perlin = GetComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 揺れ時間のカウントダウン
+        if (shakeTimer > 0)
+        {
+            shakeTimer -= Time.deltaTime;
+            if (shakeTimer <= 0)
+            {
+                StopShake();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Y))//デバッグ用
+        {
+            Shake();
+
+        }
+    }
+
+    void Shake()
+    {
+        shake.GetComponent<CinemachineBasicMultiChannelPerlin>();
+        shake.AmplitudeGain = amplitudeGain;
+        shake.FrequencyGain = frequencyGain;
+        shakeTimer = shakeDuration;
+    }
+    public void StopShake()
+    {
+        shake.AmplitudeGain = 0f;
+        shake.FrequencyGain = 0f;
+
+    }
+}
