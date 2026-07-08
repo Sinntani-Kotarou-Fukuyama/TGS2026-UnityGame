6e49b088e6dfa5ba89153ac769a49114ea616f54
Akagi-Yoshihito-Fukuyama <gc25001@anabuki-net.ne.jp>
2026-07-08 12:22:18 +0900

    Revert "怪獣がロープをくぐったときにカメラがフォーカスするようにしました。"
    This reverts commit 31c9a314951ab4a42fba0306526b6236cc08263c.

--- Tightrope Game/Assets/Script/Dino/KaijuAI.cs
+++ Tightrope Game/Assets/Script/Dino/KaijuAI.cs
@@ -21,17 +21,11 @@ public class KaijuAI : MonoBehaviour
     [Header("Patrol")]
     public Transform[] patrolPoints;
 
-    [Header("カメラを操作する用"), SerializeField] 
-    private CameraSwhich cameraSwhich;
-
-
     Animator anim;
     NavMeshAgent agent;
 
     GameObject targetBuilding;
     BreakBuilding targetBreakScript;
-
-    
 
     float attackTimer = 0f;
     bool isAttacking = false;
@@ -222,10 +216,9 @@ public class KaijuAI : MonoBehaviour
         if (collision.gameObject.tag == "RopeParts")
         {
 
-            // ロープをくぐるモーションを発動する
             anim.SetTrigger("UnderTheRope");
             agent.speed = 0.0f;
-            //cameraSwhich.DinoUnderRopeCameraSet();
+
 
         }
     }
